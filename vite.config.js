db7e978b480da50c99796850e51c0668bb6367b9
uubuge <33352557+uubuge@users.noreply.github.com>
2026-05-09 12:22:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         pricing: resolve(__dirname, 'pricing.html'),
         custom: resolve(__dirname, 'custom.html'),
         contact: resolve(__dirname, 'contact.html'),
+        alice: resolve(__dirname, '爱丽丝漫游奇遇记 (alice\'s adventures in wonderland)_001_中英_离线语音电脑/index.html'),
       },
     },
   },
