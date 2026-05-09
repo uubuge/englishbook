b6597a252896db9877d8e0e2194c7c54ab5a528d
uubuge <33352557+uubuge@users.noreply.github.com>
2026-05-09 12:35:17 +0800

    0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-const aliceDir = '爱丽丝漫游奇遇记 (alice\'s adventures in wonderland)_001_中英_离线语音电脑';
-
 export default defineConfig({
   base: '/englishbook/',
   build: {
@@ -16,7 +14,6 @@ export default defineConfig({
         pricing: resolve(__dirname, 'pricing.html'),
         custom: resolve(__dirname, 'custom.html'),
         contact: resolve(__dirname, 'contact.html'),
-        alice: resolve(__dirname, `${aliceDir}/index.html`),
       },
     },
   },
